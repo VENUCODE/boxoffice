f0aee98597a6a64bdbda2a129e58a40ddd76bcaf
venu <yedukondalunaik12345@gmail.com>
2023-08-13 19:37:05 +0530

    added the radion buttons to choose type of search(shows or actors)

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,33 +1,54 @@
 import { useState } from 'react';
-import { searchShow } from '../api/tvmaze';
+import { searchPerson, searchShow } from '../api/tvmaze';
 const Home = () => {
   const [searchInput, setsearchInput] = useState('');
   const [apiData, setApidata] = useState(null);
   const [apiDataError, setApiDataError] = useState(null);
+  const [searchOption, setSearchOption] = useState('shows');
   const inputStr = e => {
     setsearchInput(e.target.value);
   };
-  const searchStr = async (e) => {
+  const handleSearchOption = e => {
+    setSearchOption(e.target.value);
+  };
+  const searchStr = async e => {
     e.preventDefault();
     try {
       setApiDataError(null);
-      const result = await searchShow(searchInput);
-      setApidata(result)
+      if (searchOption == 'shows') {
+        const result = await searchShow(searchInput);
+        setApidata(result);
+      } else {
+        const result = await searchPerson(searchInput);
+        setApidata(result);
+    
+      }
+      
+      
     } catch (error) {
       setApiDataError(error);
     }
   };
-  
+
   const renderData = () => {
     if (apiDataError) {
+      console.log(apiDataError)
       return <div>Error: {apiDataError.message}</div>;
     }
     if (apiData) {
-      return <ol>{apiData.map(data=><li key={data.show.id}>{data.show.name}</li>)}</ol>
+      return (
+        <ol>
+          {apiData[0].person?apiData.map(data => (
+            <li key={data.person.id}>{data.person.name}</li>
+          )):apiData.map(data => (
+            <li key={data.show.id}>{data.show.name}</li>
+          ))
+          }
+        </ol>
+      );
     }
     return null;
   };
-  
 
   return (
     <div>
@@ -40,6 +61,26 @@ const Home = () => {
           onChange={inputStr}
           placeholder="Search Movie"
         />
+        <label>
+          <input
+            type="radio"
+            name="search-option"
+            onChange={handleSearchOption}
+            value="shows"
+            checked={searchOption=='shows'}
+          />
+          shows
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="search-option"
+            onChange={handleSearchOption}
+            value="actors"
+            checked={searchOption=='actors'}
+          />
+          actors
+        </label>
         <button type="submit">Search</button>
       </form>
       <div>{renderData()}</div>
